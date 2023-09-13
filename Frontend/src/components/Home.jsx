cb19d83ced3487b1a9bb399dcea8dc84def1e784
AoKuangg <camipa2305@gmail.com>
2023-09-13 09:24:52 -0500

    feat: :lipstick: Se agrega un mejor estilo para el home

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Home() {
   const location = useLocation();
-    return (
-    <h1 className='text-xs text-green-600 hover:underline'>Hi, {location.state.user.username}</h1>
-  )
+  return (
+    <>
+      <div className="relative flex flex-col justify-center min-h-screen overflow-hidden dark:bg-slate-800">
+        <h1 className="text-xl text-green-600 hover:underline text-center">
+          Hi, {location.state.user.username}
+        </h1>
+      </div>
+    </>
+  );
 }
